64a43bb4a9fb472554754b2170ab7b69d57b263c
Ion Prodan <ionprodan33@gmail.com>
2015-11-28 14:01:38 +0100

    Design and code performance

--- app.js
+++ app.js
@@ -2,26 +2,98 @@
 // Default Chrome Extension script
 (function(window) {
 	// Verify if jQuery is defined
-	if(!window.jQuery) throw "jQuery is undefined. Please include jQuery framework."
+	if(!window.jQuery) throw "jQuery is undefined. Please include jQuery framework.";
+	
+	// Number prototype for adding zero to time
+	Number.prototype.pad = function(size) {
+		var s = String(this);
+		while (s.length < (size || 2)) {
+			s = "0" + s;
+		}
+		return s;
+	}
 	
 	// Default variables
 	var $ = window.jQuery,
 		self = this,
-		playButton = $('button.play'),
-		pauseButton = $('button.pause'),
-		muteButton = $('button.mute'),
+		panelBody = $('.panel-body'),
+		filter = $('input[type=search]'),
+		playPauseButton = $('button.play_pause'),
+		radioTitle = $('h5 > .radio-title'),
+		showTime = $('h5 > small'),
 		radioContainer = $('#radio-list');
 	
 	// Window from background.html
 	self.background = chrome.extension.getBackgroundPage();
+	// Get background video
 	self.video = $(self.background.document).find('video')[0]
 	
+	// Initial function
+	if(!self.background.currentRadio) {
+		playPauseButton.prop('disabled', true).text('play_arrow')
+		radioTitle.text('Alege un radio din lista')
+		showTime.text('')
+	}
+	
+	// Default play functions
+	self.videoPlay = function() {
+		panelBody.addClass('playing')
+		playPauseButton.text('pause')
+		radioTitle.text(self.background.currentRadio.name)
+	}
+	// Default pause function
+	self.videoPause = function() {
+		panelBody.removeClass('playing')
+		playPauseButton.text('play_arrow')
+	} 
+	// Default loading start function
+	self.videoStartLoading = function() {
+		panelBody.removeClass('playing')
+		radioTitle.text('Se incarca... ' + self.background.currentRadio.name)
+		playPauseButton.prop('disabled', true)
+		showTime.text('')
+		self.background.currentRadio.loading = true;
+	}
+	// Default loading end function
+	self.videoEndLoading = function() {
+		playPauseButton.prop('disabled', false)
+		self.log("Browser has loaded the current frame");
+		self.background.currentRadio.loading = false;
+		self.video.play();
+	}
+	// Default timeupdate function
+	self.videoTimeUpdate = function() {
+		self.video.minutes = Math.floor(self.video.currentTime / 60).pad();
+		self.video.seconds = (Math.floor(self.video.currentTime) - self.video.minutes * 60).pad();
+		showTime.text(self.video.minutes + ' : ' + self.video.seconds)
+	}
+	// Default buffering function
+	self.videoBuffering = function() {
+		panelBody.removeClass('playing')
+		radioTitle.text('Buffering ...')
+	}
+	
+	// Add class playing if radio is active on document is loaded
+	if(self.video.played.length === 1 && !self.video.paused) {
+		self.videoPlay()
+	}
+	
+	// Make changes if video is paused on document is loaded
+	if(self.video.paused && !!self.background.currentRadio) {
+		self.videoPause()
+		self.videoTimeUpdate()
+		radioTitle.text(self.background.currentRadio.name)
+	}
+	
+	// Disable play/pause button if video is loading
+	if(!!self.background.currentRadio && self.background.currentRadio.loading) {
+		self.videoStartLoading()
+	}
+	
 	// Player listeners
-	playButton.click(function(){
-		return self.video.play()
-	})
-	pauseButton.click(function(){
-		return self.video.pause()
+	playPauseButton.click(function() {
+		self.video[self.video.paused ? 'play' : 'pause']()
+		$(this).text(self.video.paused ? 'play_arrow' : 'pause')
 	})
 	
 	// Operation for video, volume, pause, play, stop, etc.
@@ -29,45 +101,21 @@
 		// Alert if isset error
 		self.video.onerror = function() {
 			self.log("Error! Something went wrong", 'error');
+			// Load again
 			this.load();
 		};
-		
 		// Start loading video
-		self.video.onloadstart = function() {
-			self.log("Starting to load radio " + self.currentRadio.name);
-		};
-		
+		$(self.video).on('loadstart', self.videoStartLoading)
 		// Start when data is loaded
-		self.video.onloadeddata = function() {
-			self.log("Browser has loaded the current frame");
-			this.play();
-		};
-		
-		// Progress video
-		self.video.ontimeupdate = function() {
-			this.minutes = Math.floor(this.currentTime / 60).pad();
-			this.seconds = (Math.floor(this.currentTime) - this.minutes * 60).pad();
-			// console.log(this.minutes + ' : ' + this.seconds)
-		}
-		
+		$(self.video).on('loadeddata', self.videoEndLoading)
 		// Make changes when player begin play
-		self.video.onplay = function() {
-			playButton.addClass('disabled')
-			pauseButton.removeClass('disabled')
-			self.setActionTitle('Now playing ' + self.currentRadio.name)
-		};
-		
+		$(self.video).on('play', self.videoPlay);
 		// Make changes when player is paused
-		self.video.onpause = function() {
-			pauseButton.addClass('disabled')
-			playButton.removeClass('disabled')
-			self.setActionTitle('Now on pause ' + self.currentRadio.name)
-		};
-		
+		$(self.video).on('pause', self.videoPause)
+		// Progress video
+		$(self.video).on('timeupdate', self.videoTimeUpdate)
 		// Start when video is buffering
-		self.video.onwaiting = function() {
-			self.log("Wait! I need to buffer the next frame");
-		};
+		$(self.video).on('waiting', self.videoBuffering)
 	}
 	
 	// Basic log message
@@ -106,6 +154,10 @@
 			radioContainer.html(li).promise().done(function(){
 				// Call video listeners
 				self.videoListeners()
+				// Make current radio active
+				if(self.background.currentRadio) {
+					radioContainer.find('a[data-id=' + self.background.currentRadio.id + ']').addClass('active');
+				}
 				// Change radio
 				radioContainer.on('click', 'a', function(e) {
 					e.preventDefault();
@@ -114,14 +166,15 @@
 						// Make this channel active
 						$(this).parent().find('a').removeClass('active');
 						$(this).addClass('active');
-						// Change global current radio
-						self.currentRadio = self.radioList[$(this).data('id')];
+						// Change background global current radio
+						self.background.currentRadio = self.radioList[$(this).data('id')];
+						self.background.currentRadio.id = $(this).data('id');
 						// Stop video for change channel
-						if(self.video.src) self.video.stop()
+						if(self.video.src) self.video.pause()
 						// Make current url in background url
-						$(self.video).find('source')[0].src = self.currentRadio.url;
+						$(self.video).find('source')[0].src = self.background.currentRadio.url;
 						// Load video after src is changed
-						self.video.load();	
+						self.video.load();
 					}
 				})
 			});
@@ -131,15 +184,6 @@
 	// For reload extension
 	self.reload = function() {
 		return chrome.runtime.reload();
-	}
-	
-	// NUmber prototype for adding zero to time
-	Number.prototype.pad = function(size) {
-		var s = String(this);
-		while (s.length < (size || 2)) {
-			s = "0" + s;
-		}
-		return s;
 	}
 	
 }(window))
