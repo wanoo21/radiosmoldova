a136857c8f7a8b0fd08543ce869b8e2f6cff973a
wanoo21 <ionprodan33@gmail.com>
2016-07-02 21:56:02 +0300

    Google analytics

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-/* global chrome */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-66592876-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 // Default Chrome Extension script
 (function(global) {
     // Number prototype for adding zero to time
@@ -150,6 +159,7 @@
         radioTitle.text(self.background.currentRadio.name);
         range.val(self.background.video.volume * 100);
         range.attr('data-volume', range.val());
+        _gaq.push(['_trackEvent', self.background.currentRadio.name, 'played']);
     };
     // Default pause function
     self.videoPause = function() {
@@ -359,6 +369,7 @@
                     self.startListenFirebase();
                     // Put firebase data in DOM
                     self.getFirebaseData();
+                    _gaq.push(['_trackEvent', region, 'region']);
                     // Make current radio active
                     if(!!self.background.currentRadio) {
                         var currentRadioList = radioContainer.find('a[data-name=' + self.background.currentRadio.nameId + ']');
